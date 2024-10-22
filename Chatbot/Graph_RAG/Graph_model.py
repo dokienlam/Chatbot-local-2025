83da1e1afaa9d7f087801f1c49122d56ba63938d
Phát Trần <tranxuanphat03@gmail.com>
2024-10-22 16:26:20 +0700

    1

--- Chatbot/Graph_RAG/Graph_model.py
+++ Chatbot/Graph_RAG/Graph_model.py
@@ -37,19 +37,6 @@ def build_graph(sentences, model, nlp ):
     return graph
 
 
-def add_to_neo4j(graph, driver):
-    with driver.session() as session:
-        for node in graph.nodes(data=True):
-            entity = node[0]
-            entity_type = node[1].get("entity_type", "Unknown")
-            session.run("MERGE (e:Entity {name: $name, type: $type})", name=entity, type=entity_type)
-
-        for u, v, weight in graph.edges(data=True):
-            session.run("""
-                MATCH (e1:Entity {name: $name1}), (e2:Entity {name: $name2})
-                CREATE (e1)-[:SIMILARITY {weight: $weight}]->(e2)
-            """, name1=u, name2=v, weight=weight['weight'])
-
 def query_entities_from_neo4j(driver):
     def query_entities(tx):
         result = tx.run("MATCH (e:Entity) RETURN e.name AS name, e.type AS type")

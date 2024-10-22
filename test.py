189a4ffe533d569a13a4b24c0f29be5a9bf799bd
Phát Trần <tranxuanphat03@gmail.com>
2024-10-22 09:54:10 +0700

    a

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import json
 from gtts import gTTS
 
+path = 'C:/Users/ADMIN/Documents/GitHub/Chatbot-local-2025/responses.json'
+
 # Mở file JSON và đọc dữ liệu
-with open('du_lieu.json', 'r', encoding='utf-8') as file:
+with open(path, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Giả sử dữ liệu là một chuỗi văn bản
-text = data['noi_dung']  # Thay đổi theo cấu trúc file JSON của bạn
+text = data['responese']  # Thay đổi theo cấu trúc file JSON của bạn
 
 # Chuyển văn bản thành giọng nói
 tts = gTTS(text, lang='vi')
